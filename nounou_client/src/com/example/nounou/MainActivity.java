fdd30f1e5ca1858acca7e79a8f80474035afd268
unknown <jonathanaffre@gmail.com>
2014-05-04 10:10:17 +0200

    Added connexion checking

--- nounou_client/src/com/example/nounou/MainActivity.java
+++ nounou_client/src/com/example/nounou/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.nounou;
 
 
+import Manager.ConnectivityChangeReceiver;
 import Manager.ConnexionManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 
 public class MainActivity extends Activity {
 
@@ -15,15 +19,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		if(ConnexionManager.testConnexion(cm))
-		{
-			startActivity(new Intent(MainActivity.this, ListDesNounous.class));
-		}
-		else
-		{
-			Toast.makeText(this,"Vous devez être connecté à internet",Toast.LENGTH_LONG).show();
-		}
+		registerReceiver(
+			      new ConnectivityChangeReceiver(), 
+			      new IntentFilter(
+			            ConnectivityManager.CONNECTIVITY_ACTION));
 		
 	}
 

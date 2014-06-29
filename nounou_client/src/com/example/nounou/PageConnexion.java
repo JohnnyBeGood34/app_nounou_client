6cf6ee64d51965bd0d0d1edbfee54abd605cd12d
JohnnyBeGood34 <jonathanaffre@gmail.com>
2014-06-29 11:16:37 +0200

    GPS demande

--- nounou_client/src/com/example/nounou/PageConnexion.java
+++ nounou_client/src/com/example/nounou/PageConnexion.java
@@ -15,7 +15,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;

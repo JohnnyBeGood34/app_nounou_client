d250bf825370feeb03d10bdf2fc24f4f8774fa13
stefhack <duprestephane@hotmail.fr>
2014-07-02 15:55:41 +0200

    pull

--- nounou_client/src/com/example/nounou/UrlServer.java
+++ nounou_client/src/com/example/nounou/UrlServer.java
@@ -2,7 +2,7 @@ package com.example.nounou;
 
 public class UrlServer {
 
-	private static String url = "http://192.168.1.99:3000"; 
+	private static String url = "http://stefnodejs.azurewebsites.net"; 
 	public static String getServerUrl()
 	{
 		return url;
